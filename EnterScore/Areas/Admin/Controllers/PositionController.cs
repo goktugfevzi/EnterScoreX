53a3670dfec939d3f4778f4d45766a203d5a6261
goktugfevzi <goktugfevziozcelik@gmail.com>
2023-07-21 01:24:36 +0300

    Some Part Of Admin Panel Completed

---
+++ EnterScore/Areas/Admin/Controllers/PositionController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterScore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PositionController : Controller
+    {
+        private readonly IPositionService _positionService;
+
+        public PositionController(IPositionService positionService)
+        {
+            _positionService = positionService;
+        }
+        public IActionResult Index()
+        {
+            var values = _positionService.TGetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddPosition()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPosition(Position p)
+        {
+            _positionService.TInsert(p);
+            return RedirectToAction("Position", "Admin");
+
+        }
+        public IActionResult DeletePosition(int id)
+        {
+            var value = _positionService.TGetById(id);
+            _positionService.TDelete(value);
+            return RedirectToAction("Position", "Admin");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdatePosition(int id)
+        {
+            var value = _positionService.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdatePosition(Position p)
+        {
+
+            _positionService.TUpdate(p);
+            return RedirectToAction("Position", "Admin");
+        }
+    }
+}
--- EnterScore/Areas/Admin/Controllers/StadiumController.cs
+++ EnterScore/Areas/Admin/Controllers/StadiumController.cs
@@ -1,25 +1,28 @@
 ﻿using BusinessLayer.Abstract;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace EnterScore.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
     public class StadiumController : Controller
     {
-
         private readonly IStadiumService _stadiumService;
 
         public StadiumController(IStadiumService stadiumService)
         {
             _stadiumService = stadiumService;
         }
+
         public IActionResult Index()
         {
             var values = _stadiumService.TGetListAll();
             return View(values);
         }
+
         [HttpGet]
         public IActionResult AddStadium()
         {
@@ -27,12 +30,24 @@ namespace EnterScore.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddStadium(Stadium p)
+        public async Task<IActionResult> AddStadium(Stadium p, IFormFile imageFile)
         {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var fileName = Path.GetFileName(imageFile.FileName);
+                var physicalPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "admin_panel", "img", fileName);
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+                p.ImageURL = "/admin_panel/img/" + fileName;
+            }
+
             _stadiumService.TInsert(p);
             return RedirectToAction("Stadium", "Admin");
 
         }
+
         public IActionResult DeleteStadium(int id)
         {
             var value = _stadiumService.TGetById(id);
@@ -47,12 +62,24 @@ namespace EnterScore.Areas.Admin.Controllers
             var value = _stadiumService.TGetById(id);
             return View(value);
         }
+
         [HttpPost]
-        public IActionResult UpdateStadium(Stadium p)
+        public async Task<IActionResult> UpdateStadium(Stadium p, IFormFile imageFile)
         {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var fileName = Path.GetFileName(imageFile.FileName);
+                var physicalPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "admin_panel", "img", fileName);
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+                p.ImageURL = "/admin_panel/img/" + fileName;
+            }
 
             _stadiumService.TUpdate(p);
             return RedirectToAction("Stadium", "Admin");
         }
+
     }
 }
